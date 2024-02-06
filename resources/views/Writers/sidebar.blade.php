cb82b161fdb7d0f48194cfc8ac32e3c3d0e5f09e
Denno <dellyit001@gmail.com>
2024-02-06 01:56:58 -0800

    changes to sidebar

--- resources/views/Writers/sidebar.blade.php
+++ resources/views/Writers/sidebar.blade.php
@@ -5,13 +5,14 @@
         <nav>
             <ul class="metismenu" id="menu">
                 <li><a href="/available"><i class="fa fa-life-saver"></i> <span>Available</span></a></li>
-                <li><a href="/profile"><i class="fa fa-user-md"></i> <span>Profile</span></a></li>
-                <li><a href="/book-property"><i class="fa fa-bookmark"></i> <span>Booking</span></a></li>
-                <li><a href="/emails"><i class="fa fa-envelope"></i> <span>Emails</span></a></li>
-                <li><a href="/sms"><i class="fa fa-comment"></i> <span>SMS</span></a></li>
-                <li><a href="/permissions"><i class="fa fa-lock"></i> <span>Permissions</span></a></li>
-                <li><a href="/booked"><i class="fa fa-file-archive-o"></i> <span>Booked</span></a></li>
-                <li><a href="/tenants"><i class="fa fa-users"></i> <span>Tenants</span></a></li>
+                <li><a href="/current"><i class="fa fa-user-md"></i> <span>Current</span></a></li>
+                <li><a href="/book-property"><i class="fa fa-bookmark"></i> <span>Revision</span></a></li>
+                <li><a href="/emails"><i class="fa fa-envelope"></i> <span>Dispute</span></a></li>
+                <li><a href="/sms"><i class="fa fa-comment"></i> <span>Finished</span></a></li>
+                <li><a href="/booked"><i class="fa fa-file-archive-o"></i> <span>Bids</span></a></li>
+                <li><a href="/tenants"><i class="fa fa-users"></i> <span>Messages</span></a></li>
+                <li><a href="/tenants"><i class="fa fa-users"></i> <span>Finance</span></a></li>
+                <li><a href="/tenants"><i class="fa fa-users"></i> <span>Profile</span></a></li>
                 <li><a href="/tenants"><i class="fa fa-lock"></i> <span>Logout</span></a></li>
             </ul>
         </nav>
